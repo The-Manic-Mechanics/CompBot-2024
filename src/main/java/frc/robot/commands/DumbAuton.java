f7ae8c39cd4594cc08596bcfceccff85cb308c3b
Moostashgaming <99556514+Moostashgaming@users.noreply.github.com>
2023-03-18 08:09:30 -0400

    Daily commit

--- src/main/java/frc/robot/commands/DumbAuton.java
+++ src/main/java/frc/robot/commands/DumbAuton.java
@@ -11,7 +11,7 @@ import frc.robot.subsystems.DriveTrain;
 public class DumbAuton extends CommandBase {
   private final DriveTrain sysDriveTrain;
   private final Arm sysArm;
-  private final int auton_walk_feet = 5;
+  private final double auton_walk_feet = 4.5;
   private final boolean auton_block_placed = false;
 
   /** Creates a new DumbAuton. */
@@ -34,17 +34,21 @@ public class DumbAuton extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (!auton_block_placed) {
-
-    } else {
-      int inches = auton_walk_feet * 12;
+    // if (!auton_block_placed) {
+    //   if (sysArm.GetArmEnc() >= ) {
+    //     sysArm.SetArmSpeed(0, 0);
+    //   } else {
+    //     sysArm.SetArmSpeed(1, .40);
+    //   }
+    // } else {
+      double inches = auton_walk_feet * 12.0d;
       if ((sysDriveTrain.frontLeftEnc.getDistance() >= inches || 
          (sysDriveTrain.frontRightEnc.getDistance() >= inches) || 
           (sysDriveTrain.backLeftEnc.getDistance() >= inches) || 
           (sysDriveTrain.backRightEnc.getDistance() >= inches))) {
         sysDriveTrain.CartisianDrive(0, 0, 0);
       } else sysDriveTrain.CartisianDrive(-.5, 0, 0);
-    }
+    // }
   }
 
   // Called once the command ends or is interrupted.
