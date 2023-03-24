2bdb815f58120ab7a11bdedd1bc8c22cb5914068
stencylxd <me@stencylxd.xyz>
2023-03-24 00:18:05 +0000

    Auton and AutoAim

--- src/main/java/frc/robot/commands/AutoBalanceAuton.java
+++ src/main/java/frc/robot/commands/AutoBalanceAuton.java
@@ -1,10 +1,13 @@
 // Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
+// Open Source Software; you can modify and/or share it under t``he terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Solenoids;
 import frc.robot.subsystems.VMXPi;
@@ -15,12 +18,18 @@ import frc.robot.subsystems.VMXPi;
 public class AutoBalanceAuton extends SequentialCommandGroup {
   private final DriveTrain sysDriveTrain;
   private final VMXPi sysVMXPi;
+  private final Solenoids sysSolenoids;
+  private final Arm sysArm;
   /** Creates a new AutoBalanceAuton. */
-  public AutoBalanceAuton(DriveTrain inSysDriveTrain, VMXPi inSysVMXPi, Solenoids inSysSolenoids) {
+  public AutoBalanceAuton(DriveTrain inSysDriveTrain, VMXPi inSysVMXPi, Solenoids inSysSolenoids, Arm inSysArm) {
     sysDriveTrain = inSysDriveTrain;
     sysVMXPi = inSysVMXPi;
+    sysSolenoids = inSysSolenoids;
+    sysArm = inSysArm;
     addCommands(
-      new DriveAuton(sysDriveTrain, sysVMXPi, 5000d, -.5, 0d, 0d, true, 5),
+      new ArmDriveAuton(sysArm, sysSolenoids, ArmConstants.ARM_180_DEG, Value.kForward, 1, -.40),
+      new ArmDriveAuton(sysArm, sysSolenoids, 900, Value.kOff, 1, .40),
+      new DriveAuton(sysDriveTrain, sysVMXPi, 5000d, -.6, 0d, 0d, true, 5),
       new AutoBalance(sysVMXPi, sysDriveTrain, true, 5),
       new BrakeUp(inSysSolenoids)
     );
