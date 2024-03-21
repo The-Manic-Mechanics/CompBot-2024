bcb060135e3a78f51feddfb542fc0d164d31c53d
Moostashgaming <moostash@skiff.com>
2024-03-21 16:03:05 -0400

    good

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,15 +5,19 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.HumanInterface;
+import frc.robot.Constants.Motors.Ports;
 
 public class Intake extends SubsystemBase {
   public static class Motors {
-    public static WPI_VictorSPX left, right, lift;
+    public static WPI_VictorSPX lift;
+    public static CANSparkMax left, right;
   }
 
   public static class Encoders {
@@ -21,10 +25,13 @@ public class Intake extends SubsystemBase {
   }
 
   public Intake() {
-    Motors.left = new WPI_VictorSPX(Constants.Motors.Ports.Intake.LEFT);
-    Motors.right = new WPI_VictorSPX(Constants.Motors.Ports.Intake.RIGHT);
+    Motors.left = new CANSparkMax(Ports.Intake.LEFT, MotorType.kBrushless);
+    Motors.right = new CANSparkMax(Ports.Intake.RIGHT, MotorType.kBrushless);
+
     Motors.lift = new WPI_VictorSPX(Constants.Motors.Ports.Intake.LIFT);
+    
     Motors.right.setInverted(true);
+    
     Encoders.lift = new Encoder(Constants.Encoders.Ports.Intake.LIFT_A, Constants.Encoders.Ports.Intake.LIFT_B);
     Encoders.lift.setDistancePerPulse(Constants.Encoders.Intake.LIFT_DISTANCE_PER_PULSE);
     Encoders.lift.reset();
@@ -35,7 +42,7 @@ public class Intake extends SubsystemBase {
    */
   public static void setSpeed(double speed) {
     Motors.left.set(speed);
-    Motors.right.set(speed);
+    Motors.right.set(-speed);
   }
 
   /**
